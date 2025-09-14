b28d3717850543a444206e0449b7e486153411d5
Oussama Amara <wassa36@yahoo.fr>
2025-09-14 19:59:05 +0100

    build  code  for mutlithreading support

---
+++ src/utils/platform_thread.c
@@ -0,0 +1,32 @@
+/**
+ * @file platform_thread.c
+ * @brief Cross-platform thread abstraction for client handling.
+ *        Uses pthreads on Linux/macOS and CreateThread on Windows.
+ */
+
+#include "platform_thread.h"
+
+#ifdef _WIN32
+#include <windows.h>
+
+int create_thread(thread_t* thread, THREAD_FUNC (*func)(void*), void* arg) {
+    // Create a new thread using Windows API
+    *thread = CreateThread(NULL, 0, func, arg, 0, NULL);
+    return (*thread != NULL) ? 0 : -1;
+}
+
+void detach_thread(thread_t thread) {
+    CloseHandle(thread);
+}
+
+#else
+#include <pthread.h>
+
+int create_thread(thread_t* thread, THREAD_FUNC (*func)(void*), void* arg) {
+    return pthread_create(thread, NULL, func, arg);
+}
+
+void detach_thread(thread_t thread) {
+    pthread_detach(thread);
+}
+#endif

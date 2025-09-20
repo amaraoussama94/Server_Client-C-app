d84b5dcb66471ecc30758bdcf708c4554d18061a
Oussama Amara <wassa36@yahoo.fr>
2025-09-20 10:54:30 +0100

    Ypdate build run script

--- scripts/run_client.sh
+++ scripts/run_client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Run client with optional config
 
-CONFIG=${1:-../assets/client.cfg}
+CONFIG=${1:-../assets/client_chat.cfg}
 ../build/bin/client "$CONFIG"
--- scripts/run_server.ps1
+++ scripts/run_server.ps1
@@ -1,7 +1,5 @@
 # PowerShell - run_server.ps1
-# Exécute le serveur avec un fichier de config optionnel
+# Exï¿½cute le serveur avec un fichier de config optionnel
 
-$CONFIG = if ($args.Count -ge 1) { $args[0] } else { "../assets/server.cfg" }
-
-Write-Host "Using config file: $CONFIG"
-& "../build/bin/server.exe" $CONFIG
+CONFIG=${1:-../assets/server.cfg}
+../build/bin/server.exe "$CONFIG"

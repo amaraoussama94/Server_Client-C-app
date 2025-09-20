b19ceb8a82b2dd3f32e793f425b2105d293bde90
Oussama Amara <wassa36@yahoo.fr>
2025-09-20 10:55:08 +0100

    Ypdate build run script

--- scripts/run_client-2.ps1
+++
@@ -1,7 +0,0 @@
-# Windows PowerShell
-# Run client with optional config
-
-$CONFIG = if ($args.Count -ge 1) { $args[0] } else { "../assets/client_2.cfg" }
-
-Write-Host "Using config file: $CONFIG"
-& "../build/bin/client.exe" $CONFIG
